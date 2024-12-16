2abf5ec0a9c9fe1a97226257c32a1a1a7ed5399e
Parbat Karki <160567545+balubaiya@users.noreply.github.com>
2024-12-16 20:46:07 +0545

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,16 +6,7 @@
 
     //database connection
     $con = mysqli_connect($servername, $username, $password, $dbname);
-    //echo "connection Sucesful";
-    
-    /*if($con){
-        echo "Database connection is sucessful !!";
-    }else{
-        die(mysqli_error($con));
-        //optional no parameter: mysqli_connect_error()
-    } */
 
-    
     if(!$con){
         die(mysqli_error($con));
     } 
